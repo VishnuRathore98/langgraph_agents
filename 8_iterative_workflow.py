93ed59d69dc17cfb9ddd7a77c7f190b2c796cfca
VishnuRathore98 <rathorevishnu666@gmail.com>
2026-09-22 09:42:09 +0530

    Enhance iterative workflow with post generation, evaluation, and optimization logic

--- 8_iterative_workflow.py
+++ 8_iterative_workflow.py
@@ -4,6 +4,7 @@ from typing import Annotated, Literal, TypedDict
 from langchain_openrouter import ChatOpenRouter
 from langgraph.graph import END, START, StateGraph
 from pydantic import BaseModel, Field
+from rich import print
 
 from config import settings
 
@@ -29,18 +30,81 @@ class PostState(TypedDict):
     max_iterations: int
 
 
-class GeneratorModelSchema(BaseModel):
-    pass
-
-
 class EvaluatorModelSchema(BaseModel):
-    pass
+    evaluation: Literal["approved", "needs_improvement"]
+    feedback: str
 
 
-class OptimizerModelSchema(BaseModel):
-    pass
-
-
-post_generator_model = model.with_structured_output(GeneratorModelSchema)
 post_evaluator_model = model.with_structured_output(EvaluatorModelSchema)
-post_optimizer_model = model.with_structured_output(OptimizerModelSchema)
+
+
+def generate(state: PostState) -> dict:
+    prompt = f"You are an expert content creator write a short 10 lines long social media post for linkedin on the topic: {state['topic']}"
+    result = model.invoke(prompt).content
+
+    return {"post": result, "post_history": [result]}
+
+
+def evaluate(state: PostState) -> dict:
+    prompt = f"You are a social media post evaluator, the user has created a post on: {state['topic']}, the content is {state['post']}, evaluate the post and share your feedback and evaluation of the post as 'approved' or 'needs_improvement'.Never approve in the first evaluation."
+
+    result = post_evaluator_model.invoke(prompt)
+
+    return {
+        "feedback": result.feedback,
+        "evaluation": result.evaluation,
+        "feedback_history": [result.feedback],
+    }
+
+
+def optimize(state: PostState) -> dict:
+    prompt = f"You are a social media post optimizer, the user has written a post {state['post']}, the expert evaluator has provided its feedback on it: {state['feedback']}, now based on the feedback you have to improve upon the base post so that it can be approved by the evaluator."
+    result = model.invoke(prompt).content
+
+    current_iteration = state["current_iteration"] + 1
+
+    return {
+        "post": result,
+        "post_history": [result],
+        "current_iteration": current_iteration,
+    }
+
+
+def check_evaluation_condition(
+    state: PostState,
+) -> Literal["approved", "needs_improvement"]:
+    if (
+        state["evaluation"] == "approved"
+        or state["current_iteration"] >= state["max_iterations"]
+    ):
+        return "approved"
+    else:
+        return "needs_improvement"
+
+
+graph = StateGraph(PostState)
+
+graph.add_node("generate_post", generate)
+graph.add_node("evaluate_post", evaluate)
+graph.add_node("optimize_post", optimize)
+
+graph.add_edge(START, "generate_post")
+graph.add_edge("generate_post", "evaluate_post")
+graph.add_conditional_edges(
+    "evaluate_post",
+    check_evaluation_condition,
+    {"approved": END, "needs_improvement": "optimize_post"},
+)
+graph.add_edge("optimize_post", "evaluate_post")
+
+workflow = graph.compile()
+
+initial_state = {
+    "topic": "Value of coins in the Indian market.",
+    "max_iterations": 5,
+    "current_iteration": 1,
+}
+
+result = workflow.invoke(initial_state)
+
+print(result)
